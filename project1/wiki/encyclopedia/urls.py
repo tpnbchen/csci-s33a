6104c60759bd99d8c265eca554d851dcb61c921a
byron <byronyi@gmail.com>
2024-07-05 02:15:57 -0400

    entry and index page specs complete

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/page", views.page, name="page"),
+    path("wiki/<str:title>", views.entry, name="entry")
 ]
+
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from markdown2 import markdown_path
 from . import util
 
 
@@ -7,4 +8,26 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def page(request):
+    return render(request, "encyclopedia/page.html", {
+        "items":[1, 2, 3]
+    })
+
+
+def entry(request, title):
+    if util.get_entry(title):
+        html = markdown_path(f"entries/{title}.md")
+        return render(request, "encyclopedia/entry.html", {
+            "title":title,
+            "content": html
+        })
+    else:
+        return render(request, "encyclopedia/entry404.html", {
+            "title":title
+        })
+    # return HttpResponse(f"Entry, {title}")
+
+
 

6f5158d8061000189eb5f3195f95e155c86c6316
byron <byronyi@gmail.com>
2024-08-08 11:11:20 -0400

    start templates and statics

--- projectfinal/projectfinal/hanabi/models.py
+++ projectfinal/projectfinal/hanabi/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Game(models.Model):
+    name = models.CharField(max_length=25)
+    started = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField()
+    score = models.PositiveSmallIntegerField(default=0,validators=[MaxValueValidator(25)])
+    hints = models.PositiveSmallIntegerField(default=8,validators=[MaxValueValidator(8)])
+    fuses = models.PositiveSmallIntegerField(default=3,validators=[MaxValueValidator(3)])
+
+class Deck(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    cards = models.JSONField()
+
+class GamePlayer(models.Model):
+    player = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    hand = models.JSONField()
--- projectfinal/projectfinal/hanabi/views.py
+++ projectfinal/projectfinal/hanabi/views.py
@@ -1,7 +1,81 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+
+from .models import User
 
 def index(request):
     return HttpResponse("hello world!")
 
-def 
+
+# login
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse(reverse("index"))
+        else:
+            return render(request, "hanabi/login.html", {
+                "message": "invalid username and/or password"
+            })
+    else:
+        return render(request, "hanabi.login.html")
+
+
+# logout
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+# new user
+def new_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        password = request.POST["password"]
+        confirmation = request.POST["password"]
+        if confirmation != password: 
+            return render(request, "network/register.html", {
+                "message": "Passwords do not match."
+            })
+        
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already exists."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
+
+
+# rules page
+def rules(request):
+    return HttpResponse("Rules")
+
+
+# start a new game
+def start_game(request):
+    pass
+
+
+# join a game
+def join_game(request):
+    pass
+
+
+# take a turn
+def take_turn(request):
+    pass
